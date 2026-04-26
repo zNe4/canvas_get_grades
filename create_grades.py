88f0fcf46763466aba852f1b13b24c2b6334b4d1
zNe4 <oneyra@uc.cl>
2026-04-26 01:28:51 -0400

    remade the code for creating grades so it handles name changes between platforms

--- create_grades.py
+++ create_grades.py
@@ -1,58 +1,31 @@
 import numpy as np
 import pandas as pd
-import unicodedata
-from collections import defaultdict
-
-
-def clean_string(chain): # Removes accents
-    return ''.join((c for c in unicodedata.normalize('NFD',chain) if unicodedata.category(c) != 'Mn')).lower()
-
 
 # Get the important stuff from the dataframe
 df = pd.read_csv("raw_student_grades.csv", sep=",", header=0)
 df.drop(index=df.index[0], axis=0, inplace=True)
-# print(df.head())
-l = list(df)
-# print(l)
-print('\n\tWhat test do you want to grade?\n')
-for i in range(1, len(l)):
+l = list(df) # This is a list of the headers of df
+print('\n\tWhich test do you want to grade?\n')
+for i in range(4, len(l)):
     print(f'{i}: {l[i]}')
-p = int(input('\n\tI want to grade test number '))
-df = df[[l[0], l[p]]]
+p = int(input('\n\tI want to grade test number ')) # This is the index the user wants to grade
+df = df[[l[2], l[p]]]
 df.rename(columns={
-    l[0]: 'name',
+    l[2]: 'login',
     l[p]: 'score'
     }, inplace=True)
 
-# df = df[['name', 'score']]
-# p = input('\nDoes the test accept multiple attempts? [y/n] ')
-# if p == 'y':
-#     df = df.sort_values(['name', 'score'], ascending=False).groupby('name').head(1)
-df['name'] = df.apply(lambda x: ''.join(x['name'].split('-')), axis=1)
-df['name'] = df.apply(lambda x: ''.join(x['name'].split(',')), axis=1)
 df['score'] = df['score'].fillna(0)
-# print(df.head())
 
+# Now df is just two columns with the login (text before the @ in the mail) and the score of the test
 
 df2 = pd.read_csv("all_students.csv", sep=",", header=0)
-df2.rename(columns={'Nombre Estudiante': 'name'}, inplace=True)
-df2 = df2[['name']]
+df2.rename(columns={'Email': 'login', 'Nombre Estudiante': 'name'}, inplace=True)
+df2 = df2[['name', 'login']] # So df2 is the name and login given by labmat of the student
+df2['login'] = df2['login'].apply(lambda x: x.split('@')[0]) # Get only the name and not the domain
 
-df2['name'] = df2.apply(lambda x: ''.join(x['name'].split('-')), axis=1)
-names_array = [
-        ' '.join(sorted(clean_string(x).split(' '))) for x in df['name'].tolist()
-]
-# Now each element in the array is the sorted name (unique) of a student.
-scores_array= df['score'].tolist()
-d = defaultdict(int)
-for i in range(len(names_array)):
-    d[names_array[i]] = round(float(scores_array[i]), 2)
-
-# And d is the dict containing the scores of the unique sorted name
-
-df2['score'] = df2.apply(lambda x: d[' '.join(sorted(clean_string(x['name']).split(' ')))], axis=1)
-
+df2 = pd.merge(df2, df, on='login', how='inner')
+df2.drop(columns=['login'], inplace=True)
 df2.to_csv('students_grades.csv', index=False)
-# print(df2.head())
 print('Done!')
 
